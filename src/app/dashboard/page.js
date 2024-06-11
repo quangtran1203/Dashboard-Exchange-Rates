30f988c549b27b646c3d6a3f0d2de7be12276954
quangtran1203 <harrytran1232001@gmail.com>
2024-06-11 14:40:37 +0700

    fix: fixed component rerendering errors

--- src/app/dashboard/page.js
+++ src/app/dashboard/page.js
@@ -2,7 +2,14 @@
 import React, { useEffect, useState } from "react";
 import ExchangeRateChart from "../../../components/ExchangeRateChart";
 import styles from "./dashboard.module.css";
-import { Typography, Select, InputLabel, MenuItem, FormControl, FormHelperText } from "@mui/material";
+import {
+    Typography,
+    Select,
+    InputLabel,
+    MenuItem,
+    FormControl,
+    FormHelperText,
+} from "@mui/material";
 import ExchangeRateTable from "../../../components/ExchangeRateTable";
 import BackdropLoading from "../../../components/Backdrop";
 import { formatRequest } from "../../../utils/formatRequest";
@@ -10,7 +17,7 @@ import Conversion from "../../../components/Conversion";
 
 const App = () => {
     const [data, setData] = useState({ dates: [], cad: [], usd: [], eur: [] });
-    const [baseCurrency, setBaseCurrency] = useState(!localStorage.getItem("baseCurrency") ? "USD" : localStorage.getItem("baseCurrency"));
+    const [baseCurrency, setBaseCurrency] = useState("");
     const [loading, setLoading] = useState(false);
     const [dataReady, setDataReady] = useState(false);
 
@@ -19,49 +26,58 @@ const App = () => {
     pastDate.setFullYear(pastDate.getFullYear() - 2);
     const formatDate = (date) => {
         const year = date.getFullYear();
-        const month = String(date.getMonth() + 1).padStart(2, '0');
-        const day = String(date.getDate()).padStart(2, '0');
+        const month = String(date.getMonth() + 1).padStart(2, "0");
+        const day = String(date.getDate()).padStart(2, "0");
         return `${year}-${month}-${day}`;
     };
     const pastDateFormatted = formatDate(pastDate);
 
     const fetchExchangeRates = async (baseCurrency) => {
         try {
-            setLoading(true);
-            setDataReady(false);
-            const response = await fetch(
-                `https://api.frankfurter.app/${pastDateFormatted}..?from=${baseCurrency}&to=USD,CAD,EUR`
-            );
-            const result = await response.json();
+            if (baseCurrency !== "") {
+                setLoading(true);
+                setDataReady(false);
+                const response = await fetch(
+                    `https://api.frankfurter.app/${pastDateFormatted}..?from=${baseCurrency}&to=USD,CAD,EUR`
+                );
+                const result = await response.json();
 
-            const dates = [];
-            const cad = [];
-            const eur = [];
-            const usd = [];
+                const dates = [];
+                const cad = [];
+                const eur = [];
+                const usd = [];
 
-            for (const [date, rates] of Object.entries(result.rates)) {
-                dates.push(date);
-                rates.CAD && cad.push(rates.CAD);
-                rates.EUR && eur.push(rates.EUR);
-                rates.USD && usd.push(rates.USD);
+                for (const [date, rates] of Object.entries(result.rates)) {
+                    dates.push(date);
+                    rates.CAD && cad.push(rates.CAD);
+                    rates.EUR && eur.push(rates.EUR);
+                    rates.USD && usd.push(rates.USD);
+                }
+
+                setData({ dates, cad, eur, usd });
+
+                // format req body data to make a POST req to backend server
+                const formattedData = formatRequest(
+                    baseCurrency,
+                    { dates, cad, eur, usd },
+                    currentDate.toISOString()
+                );
+                const resFromBackend = await fetch(
+                    "http://localhost:8000/api/exchange-rate-data",
+                    {
+                        method: "POST",
+                        headers: {
+                            "Content-Type": "application/json",
+                        },
+                        body: JSON.stringify(formattedData),
+                    }
+                );
+                const resultFromBackend = await resFromBackend.json();
+                console.log(resultFromBackend);
+
+                setLoading(false);
+                setDataReady(true);
             }
-
-            setData({ dates, cad, eur, usd });
-
-            // format req body data to make a POST req to backend server
-            const formattedData = formatRequest(baseCurrency, { dates, cad, eur, usd }, currentDate.toISOString());
-            const resFromBackend = await fetch("http://localhost:8000/api/exchange-rate-data", {
-                method: "POST",
-                headers: {
-                    "Content-Type": "application/json"
-                },
-                body: JSON.stringify(formattedData)
-            });
-            const resultFromBackend = await resFromBackend.json();
-            console.log(resultFromBackend);
-
-            setLoading(false);
-            setDataReady(true);
         } catch (error) {
             console.error("Error fetching exchange rates:", error);
             setLoading(false);
@@ -69,13 +85,16 @@ const App = () => {
     };
 
     useEffect(() => {
+        if (localStorage.getItem("baseCurrency")) {
+            setBaseCurrency(localStorage.getItem("baseCurrency"));
+        }
         fetchExchangeRates(baseCurrency);
     }, [baseCurrency]);
 
     const handleCurrencyChange = (event) => {
         setBaseCurrency(event.target.value);
         localStorage.setItem("baseCurrency", event.target.value);
-    }
+    };
 
     return (
         <div className={styles.dashboardWrapper}>
@@ -95,7 +114,6 @@ const App = () => {
                             labelId="select-label"
                             id="simple-select"
                             value={baseCurrency}
-                            defaultValue="USD"
                             label="Base Currency"
                             onChange={handleCurrencyChange}
                         >
@@ -103,18 +121,26 @@ const App = () => {
                             <MenuItem value={"CAD"}>CAD</MenuItem>
                             <MenuItem value={"EUR"}>EUR</MenuItem>
                         </Select>
-                        <FormHelperText>Convert 1 {baseCurrency} to other currencies</FormHelperText>
+                        <FormHelperText>
+                            Convert 1 {baseCurrency} to other currencies
+                        </FormHelperText>
                     </FormControl>
 
-                    {
-                        dataReady && <Conversion baseCurrency={baseCurrency} />
-                    }
+                    {dataReady && baseCurrency !== "" && <Conversion baseCurrency={baseCurrency} />}
                 </div>
 
-                <div className={styles.chartWrapper}><ExchangeRateChart data={data} /></div>
-            </div>
+                {baseCurrency !== "" && (
+                    <div className={styles.chartWrapper}>
+                        <ExchangeRateChart data={data} />
+                    </div>)
+                }
 
-            <ExchangeRateTable data={data} />
+            </div>
+            {
+                baseCurrency !== "" && (
+                    <ExchangeRateTable data={data} />
+                )
+            }
         </div>
     );
 };
